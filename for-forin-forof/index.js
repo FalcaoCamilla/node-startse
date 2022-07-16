96fd71220adb1106d2d86000b6b8c02e83afa325
camilla.falcao <millafalcao.acad@gmail.com>
2022-07-16 03:24:46 -0300

    map, filter and reduce

--- for-forin-forof/index.js
+++ for-forin-forof/index.js
@@ -9,9 +9,35 @@ async function main(){
     //     console.log(starWarsPeople[0][propPerson])
     // }
 
-    for(let person of starWarsPeople){
-        console.log(person.name)
-    }
+    // for(let person of starWarsPeople){
+    //     console.log(person.name)
+    // }
+
+    //novo array com map
+    const starWarsPeopleNames = starWarsPeople.map((person) => {
+        const {name, height, gender} = person; //boa prática. linhas 18 e 19
+        // const name = person.name;
+        // const height = person.height
+        return {name, height, gender} 
+    });
+    console.log(starWarsPeopleNames)
+
+    //filtros no array
+    const starWarsPeopleFiltered = starWarsPeople.filter(person => person.gender === 'male')
+    .map((person) => {
+       return {name: person.name, gender: person.gender} 
+    } )
+    console.log(starWarsPeopleFiltered)
+
+    //manipulando array com reduce, retornando soma
+    const totalHeight = starWarsPeople.reduce((total, person) => { //primeiro parametro - acumulador. segundo parametro - elemento que esta sendo iterada
+        total += Number(person.height);
+
+        return total
+    }, 0)
+    
+    console.log('Media das alturas', totalHeight/starWarsPeople.length);
+
 }
 
 main();
